695304769d5c1b842819c805517ce22028e8e29f
Victoria Vichet <45668496+Victoria-Vichet@users.noreply.github.com>
2019-05-19 01:16:45 +0200

    Update index.php

--- projet/index.php
+++ projet/index.php
@@ -1,11 +1,34 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Accueil</title>
+  </head>
     <body>
-        <?php
+     <?php
             header('Location: controller/routeur.php?controller=produit&&action=search');
         ?>
-    </body>
+    	<center>
+    		<h1>Que manger devant Breaking Bad ?</h1>
+    		<h2>Projet tutoré de DUT Informatique</h2></br>
+    		<h3>
+    			<i>Réalisé dans le cadre de l'Année Spéciale par: </br> 
+	    		Julien Alauzet </br> 
+	    		Guillaume Dufour </br> 
+	    		Thibault Matthews </br> 
+	    		Victoria Vichet </br>
+	    		</i>
+	    	</h3>
+    	</center>
+    	</br>
+    	<p align="justify">	Vous venez de passer une journée éreintante au travail, vous n’avez plus qu’une seule envie - rentrer chez vous - et vous pensez déjà à votre soirée canapé devant une bonne série et avec un bon repas. </br>
+		Qu’est-ce que je vais bien pouvoir manger ? </br>
+		Qu’est-ce que je vais bien pouvoir regarder ? </br>
+		Nous vous proposons de ne plus vous infliger le stress de devoir décider. Avec l’aide de notre base de données, nous serons en mesure de vous recommander la série parfaite ou le repas idéal, en fonction de vos besoins. </br></br>
+
+    	Sinon on peut mettre un résumé plutôt professionnel type résumé de rapport. </br>
+    	</p>
+	</body>
+	<footer>
+		<p>Année universitaire 2018-2019 - IUT Montpellier Sète</p>
+	</footer>
 </html>
